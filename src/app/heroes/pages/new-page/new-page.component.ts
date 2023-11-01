4b19c7441f8d981151919ab921e395d55c0a7cb5
ChristianTC <christiantambo15@gmail.com>
2023-11-01 11:46:49 -0400

    add Snackbars and redirect to other pages

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -4,6 +4,7 @@ import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-page',
@@ -31,7 +32,8 @@ export class NewPageComponent implements OnInit {
   constructor(
     private heroesService: HeroesService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private snackbar: MatSnackBar
     ) {}
 
   get currentHero(): Hero {
@@ -59,7 +61,7 @@ export class NewPageComponent implements OnInit {
     if (this.currentHero.id) {
       this.heroesService.updateHero( this.currentHero )
         .subscribe(hero => {
-          // TODO show snackbar
+          this.showSnackbar(`${hero.superhero} updated`)
         })
 
       return
@@ -67,10 +69,16 @@ export class NewPageComponent implements OnInit {
 
     this.heroesService.addHero( this.currentHero )
       .subscribe( hero => {
-        // TODO show snackbar, and navigate to /heroes/edit/hero.id
+        this.router.navigate(['/heroes/edit', hero.id])
+        this.showSnackbar(`${hero.superhero} created`)
       })
-    // this.heroesService.updateHero(this.heroForm.value)
 
+  }
+
+  showSnackbar(message: string):void {
+    this.snackbar.open(message, 'done', {
+      duration: 2500,
+    })
   }
 
 }
